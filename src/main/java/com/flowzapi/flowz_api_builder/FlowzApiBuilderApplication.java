b91f8cb5e6a8fe719911c593bde7329b749ecf97
yinoni <yinonmegidesh2212@gmail.com>
2026-05-20 12:32:50 +0300

    First commit

---
+++ src/main/java/com/flowzapi/flowz_api_builder/FlowzApiBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.flowzapi.flowz_api_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowzApiBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowzApiBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowzapi/flowz_api_builder/FlowzApiBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowzapi.flowz_api_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowzApiBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
